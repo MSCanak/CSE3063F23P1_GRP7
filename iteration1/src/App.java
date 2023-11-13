01d0ad02a9ffc0f2987cf5c407de74b268cccb03
Mustafa Said Çanak <mscanak0@gmail.com>
2023-11-13 19:23:48 +0300

    main file and Person class created

---
+++ iteration1/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        // Create Login object and login
+        LoginInterface loginInterface = new LoginInterface();
+        loginInterface.login();
+    }
+}
