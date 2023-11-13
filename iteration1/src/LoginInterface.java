0a287bcc922337f2e76dba9abc8addb7bb5282b5
Mustafa Said Çanak <mscanak0@gmail.com>
2023-11-14 00:10:43 +0300

    LoginInterface class created

---
+++ iteration1/src/LoginInterface.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class LoginInterface {
+    
+    // Attributes
+    private Person person;
+    private Scanner scanner;
+
+    public void login() {
+
+    }
+
+    public void logout() {
+
+    }
+
+    public void exit() {
+
+    }
+
+    private void getLoginInf() {
+
+    }
+
+    private void searchUser() {
+
+    }
+}
