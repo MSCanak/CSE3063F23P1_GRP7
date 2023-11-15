2782e4fe46cd80cc4b19d1d8dd48cf14f11f3cca
Muhammed Karagul <talhakaragul@marun.edu.tr>
2023-11-15 20:17:24 +0300

    json data pull added

--- iteration1/src/Course.java
+++ iteration1/src/Course.java
@@ -2,6 +2,12 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
+  
+import org.json.simple.JSONArray; 
+import org.json.simple.JSONObject; 
+import org.json.simple.parser.*; 
+
+
 public class Course {
     
     // initialize variables
@@ -50,13 +56,34 @@ public class Course {
         this.mandatoryPrerequisite = mandatoryPrerequisite;
     }
 
-        private void showCourses() {
+    public void showCourses() throws Exception {
         
         System.out.printf("%-10s%-35s%-15s%-8s%-10s%-25s%-25s%n",
                 "CourseID", "CourseName", "CourseType", "Credit", "Semester",
                 "OptionalPrerequisites", "MandatoryPrerequisites"); //aralarına | koyup tablo gibi yapabilirim
 
+        Object coursesJSONobj;
+        JSONArray courseJSONarr;
+        coursesJSONobj = new JSONParser().parse(new FileReader("./jsons/courses.json")); 
+
+        courseJSONarr = (JSONArray) coursesJSONobj;
+
+        for (Object courseObj: courseJSONarr) {
+            JSONObject course = (JSONObject) courseObj;
+
+            String CourseID = (String) course.get("CourseID"); 
+            String CourseName = (String) course.get("CourseName");
+            String CourseType = (String) course.get("CourseType");
+            Integer Credit = (int) course.get("Credit");
+            Integer Semester = (int) course.get("Semester");
+
+            System.out.printf("Semester %d%n", Semester);
+            System.out.printf("\t%1s%10s%1s%35s%1s%15s%1s%8s%1s%10s%1s%25s%1s%25s%n", "|",
+                    CourseID,"|", CourseName, "|", CourseType,"|" ,Credit,"|",
+                    "OptionalPrerequisites","|", "MandatoryPrerequisites");
 
     }
 
 }
+}
+
