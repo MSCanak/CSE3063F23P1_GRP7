7b359680a64dde802ef2ce788af12c0772531888
0xEnsar <ensaryozgat@gmail.com>
2023-11-15 23:54:01 +0300

    advRegMenu, showStudents method improvements

--- src/AdvisorCourseRegistrationInterface.java
+++ src/AdvisorCourseRegistrationInterface.java
@@ -5,22 +5,23 @@ public class AdvisorCourseRegistrationInterface {
     private AdvisorInterface advisorInt;
     private Scanner scanner;
 
-    public AdvisorCourseRegistrationInterface(Advisor advisor, AdvisorInterface advisorInt) {
+    public AdvisorCourseRegistrationInterface(Advisor advisor/* , AdvisorInterface advisorInt*/) {
         this.advisor = advisor;
-        this.advisorInt = advisorInt;
+        // this.advisorInt = advisorInt;
         this.scanner = new Scanner(System.in);
     }
 
     public void advRegMenu() {
-        System.out.println("Welcome to the Advisor Course Registration Interface");
-        System.out.println("Please select an option:");
-        System.out.println("1. Show Students");
-        System.out.println("2. Approve/Deny Courses");
-        System.out.println("3. Finalize Registration");
-        System.out.println("4. Go Back to Main Menu");
-
-        int choice = scanner.nextInt();
-        switch (choice) {
+        
+        while(true) {
+            System.out.println("Welcome to the Advisor Course Registration Interface");
+            System.out.println("Please select an option:");
+            System.out.println("1. Show Students");
+            System.out.println("2. Approve/Deny Courses");
+            System.out.println("3. Finalize Registration");
+            System.out.println("4. Go Back to Main Menu");
+            int choice = scanner.nextInt();
+            switch (choice) {
             case 1:
                 showStudents();
                 break;
@@ -36,17 +37,34 @@ public class AdvisorCourseRegistrationInterface {
                 System.out.println("Invalid choice. Please try again.");
                 advRegMenu();
                 break;
+            }
         }
 
     }
 
     private void showStudents() {
+        System.out.println("//////////////////////////////////");
+        int numberOfStudents = 1;
+
+        // System.out.println(advisor.getStudents().size());
         for (Student student : advisor.getStudents()) {
-            System.out.println(student.getName() + " " + student.getSurname() + " " + student.getID());
+            System.out.println(numberOfStudents + " : " + student.getName() + " " + student.getSurname() + " " + student.getID());
+            
         }
+        System.out.println("//////////////////////////////////");
+        return;
     }
-    private void showStudentsRequested() {}
-    private void approveCoursesMenu() {}
+    private void showStudentsRequested() {
+       
+    }
+    private void approveCoursesMenu() {
+        showStudents();
+        int choice = scanner.nextInt();
+        
+        Student student = advisor.getStudents().get(choice-1);
+        System.out.println("//////////////////////////////////");
+        System.out.println("Student: " + student.getName() + " " + student.getSurname() + " " + student.getID());
+    }
     private void showStudentsCourses() {}
     private void saveApprovel() {}
     private void showStudentsApproved() {}
--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class App {
 
     public static void main(String[] args) throws Exception {
@@ -6,5 +8,15 @@ public class App {
 
         Person person = json.searchPerson("150101");
         System.out.println(person.getID());
+
+        Person student = json.searchPerson("150120000");
+        System.out.println(student.getID());
+
+        ArrayList<Student> students = new ArrayList<Student>();
+        students.add((Student)student);
+        person.setStudents(students);
+
+        AdvisorCourseRegistrationInterface advRegInt = new AdvisorCourseRegistrationInterface((Advisor) person);
+        advRegInt.advRegMenu();
     }
 }
