3829598347a1a56de59a3ed9b2437fd724a75fc9
Mustafa Said Çanak <mscanak0@gmail.com>
2023-11-17 15:58:43 +0300

    constructor corrected

--- src/Advisor.java
+++ src/Advisor.java
@@ -5,8 +5,7 @@ class Advisor extends Person {
     private ArrayList<Student> students = new ArrayList<Student>();
 
     public Advisor(String name, String surname, String email, String phoneNumber, String faculty, String department, String ID, String password){
-        super(name, surname, ID, password, email, phoneNumber, faculty, department);
-        this.students = students;
+        super(name, surname, email, phoneNumber, ID, password, faculty, department);
     }
 
     public ArrayList<Student> getStudents(){ 
