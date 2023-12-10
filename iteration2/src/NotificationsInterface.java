22b4241514e70c6d800c9fbeac14e07280574b60
Muhammed Karagul <talhakaragul@marun.edu.tr>
2023-12-10 22:18:59 +0300

    NotificationMenu interface  changed into NotificationInterface class

---
+++ iteration2/src/NotificationsInterface.java
@@ -0,0 +1,14 @@
+public class NotificationsInterface {
+    // shows the notifications
+    public void showNotifications(){
+
+    }
+    // calculates the notifications by reading json file
+    public void calculateNotifications(){
+        
+    }
+    // shows the notifications menu and let user take action in notifications
+    public void notificationsMenu(){
+        
+    }
+}
