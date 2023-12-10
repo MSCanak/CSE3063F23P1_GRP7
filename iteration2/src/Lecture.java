f01aabc27522163979b5e81fb1727d730d0cbc63
Muhammed Nael <imohmad7@gmail.com>
2023-12-10 18:26:12 +0300

    added Lecture.java

---
+++ iteration2/src/Lecture.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Lecture extends Course {
+
+    String lectureCourseId;
+
+    public Lecture(String lectureCourseId, String courseName, String courseID, String type, int credit,
+                   int theoric, int practice, int quota, ArrayList<Course> optionalPrerequisite,
+                   ArrayList<Course> mandatoryPrerequisite, ArrayList<Student> courseStudent, Lecturer lecturer) {
+        super(courseName, courseID, type, credit, theoric, practice, quota, optionalPrerequisite, mandatoryPrerequisite, courseStudent, lecturer);
+        this.lectureCourseId = lectureCourseId;
+    }
+
+    public String getLectureCourseId() {
+        return lectureCourseId;
+    }
+
+    public void setLectureCourseId(String lectureCourseId) {
+        this.lectureCourseId = lectureCourseId;
+    }
+}
