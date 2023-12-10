2e0c890cfcdfe4a541da24e68f28f1c6e7e81222
Muhammed Karagul <talhakaragul@marun.edu.tr>
2023-12-10 15:46:08 +0300

    showCurriculum completed

--- iteration2/src/Colors.java
+++ iteration2/src/Colors.java
@@ -1,10 +1,11 @@
 public class Colors {
     public static final String RESET = "\u001B[0m";
     public static final String BLACK = "\u001B[30m";
-    public static final String RED = "\u001B[31m";
-    public static final String GREEN = "\u001B[32m";
-    public static final String YELLOW = "\u001B[33m";
-    public static final String BLUE = "\u001B[34m";
+    public static final String RED = "\u001B[31m"; // using for title
+    public static final String GREEN = "\u001B[32m"; // using for warning
+    public static final String YELLOW = "\u001B[33m"; // using for subtitle
+    public static final String BLUE = "\u001B[34m"; //using for message
+    public static final String GRAY = "\u001B[90m"; // using for 
     public static final String PURPLE = "\u001B[35m";
     public static final String CYAN = "\u001B[36m";
     public static final String WHITE = "\u001B[37m";
--- iteration2/src/StudentInterface.java
+++ iteration2/src/StudentInterface.java
@@ -7,6 +7,7 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+
 public class StudentInterface {
 
     // attributes
@@ -14,11 +15,12 @@ public class StudentInterface {
     private LoginInterface loginInterface;
     private StudentCourseRegistrationInterface studentCourseRegistrationInterface;
     Scanner input;
+
     // constructor
     public StudentInterface(Student person, LoginInterface loginInterface) {
         this.student = person;
         this.loginInterface = loginInterface;
-       
+
     }
 
     public void stuMenu() {
@@ -33,116 +35,115 @@ public class StudentInterface {
         System.out.println("Enter 'x' to exit");
 
         input = new Scanner(System.in);
-        char choice  = input.next().charAt(0);
-        
+        char choice = input.next().charAt(0);
 
         switch (choice) {
-        
-        // viewing transcript
-        case '1':
-            student.getTranscript().viewTranscript();
-            boolean a = true;
-            while (a) {
-                System.out.println("Enter '0' to go back to the main menu.");
-                char backChoice = input.next().charAt(0);
-                if (backChoice == '0') {
-                    stuMenu();
-                } else {
-                    System.out.println("Invalid input. Please try again.");
+
+            // viewing transcript
+            case '1':
+                student.getTranscript().viewTranscript();
+                boolean a = true;
+                while (a) {
+                    System.out.println("Enter '0' to go back to the main menu.");
+                    char backChoice = input.next().charAt(0);
+                    if (backChoice == '0') {
+                        stuMenu();
+                    } else {
+                        System.out.println("Invalid input. Please try again.");
+                    }
                 }
-            }
 
-        // show courses
-        case '2':
-        try {
-            showCurriculum();
-        } catch (FileNotFoundException e) {
-        } catch (IOException e) {
-        } catch (ParseException e) {
-        }
-            break;
+                // show courses
+            case '2':
+                try {
+                    showCurriculum();
+                } catch (FileNotFoundException e) {
+                } catch (IOException e) {
+                } catch (ParseException e) {
+                }
+                break;
 
-        // going to course registration system
-        case '3':
-        studentCourseRegistrationInterface = new StudentCourseRegistrationInterface(student,this);
-        studentCourseRegistrationInterface.stuRegMenu();
-            break;
-        
-        // logging out
-        case '*':
-        loginInterface.logout();
-            break;
+            // going to course registration system
+            case '3':
+                studentCourseRegistrationInterface = new StudentCourseRegistrationInterface(student, this);
+                studentCourseRegistrationInterface.stuRegMenu();
+                break;
 
-        // exiting
-        case 'x':
-        System.out.println("\n-----------------------Thank you for using Marmara Course Registration System-----------------------");
-        loginInterface.exit();
-            break;
-        
-        // invalid input
-        default:
-        System.out.println("Invalid input.Please try again.");
-        stuMenu();
-            break;
+            // logging out
+            case '*':
+                loginInterface.logout();
+                break;
+
+            // exiting
+            case 'x':
+                System.out.println(
+                        "\n-----------------------Thank you for using Marmara Course Registration System-----------------------");
+                loginInterface.exit();
+                break;
+
+            // invalid input
+            default:
+                System.out.println("Invalid input.Please try again.");
+                stuMenu();
+                break;
         }
     }
+
     private void showCurriculum() throws FileNotFoundException, IOException, ParseException {
         Object coursesJSONobj;
-            JSONArray courseJSONarr;
-            coursesJSONobj = new JSONParser().parse(new FileReader("./jsons/courses.json"));
-    
-            courseJSONarr = (JSONArray) coursesJSONobj;
-    
-            int previousSemester = -1;
-            for (Object courseObj : courseJSONarr) {
-                JSONObject course = (JSONObject) courseObj;
-    
-                String CourseID = (String) course.get("CourseID");
-                String CourseName = (String) course.get("CourseName");
-                String CourseType = (String) course.get("CourseType");
-                long Credit = (long) course.get("Credit");
-                long Semester = (long) course.get("Semester");
-                JSONArray OptionalPrerequisites = (JSONArray) course.get("OptionalPrerequisites");
-                JSONArray MandatoryPrerequisites = (JSONArray) course.get("MandatoryPrerequisites");
-    
-                if (CourseType.equals("M")) {
-                    CourseType = "Mandatory";
-                } else if (CourseType.equals("E")) {
-                    CourseType = "Elective";
-                }
-    
-                String optionalPrerequisitesString = OptionalPrerequisites.toString().replace("\"", "").replace("[", "")
-                        .replace("]", "").replace(", ", "-");
-                String mandatoryPrerequisitesString = MandatoryPrerequisites.toString().replace("\"", "").replace("[", "")
-                        .replace("]", "").replace(", ", "-");
-    
-                if (previousSemester != Semester) {
-                    System.out.printf(
-                            "-----------------------------------------------------------------------------------------------------------------------------------------------------------------%n");
-                    System.out.printf("Semester %s", Semester);
-                    System.out.printf("\t%-10s%-45s%-15s%-8s%-35s%-35s%n%n",
-                            "CourseID", "CourseName", "CourseType", "Credit",
-                            "OptionalPrerequisites", "MandatoryPrerequisites");
-                    previousSemester = (int) Semester;
-                }
-    
-                System.out.printf("\t\t%-10s%-45s%-15s%-8s%-35s%-35s%n",
-                        CourseID, CourseName, CourseType, Credit,
-                        optionalPrerequisitesString, mandatoryPrerequisitesString);
-        
-    
-            }
-            boolean a = true;
-            while (a) {
-                System.out.println("Enter '0' to go back to the main menu.");
-                input = new Scanner(System.in);
-                char backChoice = input.next().charAt(0);
-                if (backChoice == '0') {
-                    stuMenu();
-                } else {
-                    System.out.println("Invalid input. Please try again.");
-                }
-            }
-    }       
-}
+        JSONArray courseJSONarr;
+        coursesJSONobj = new JSONParser().parse(new FileReader("./jsons/courses.json"));
 
+        courseJSONarr = (JSONArray) coursesJSONobj;
+
+        int previousSemester = -1;
+        for (Object courseObj : courseJSONarr) {
+            JSONObject course = (JSONObject) courseObj;
+
+            String CourseID = (String) course.get("CourseID");
+            String CourseName = (String) course.get("CourseName");
+            String CourseType = (String) course.get("CourseType");
+            long Credit = (long) course.get("Credit");
+            long Semester = (long) course.get("Semester");
+            JSONArray OptionalPrerequisites = (JSONArray) course.get("OptionalPrerequisites");
+            JSONArray MandatoryPrerequisites = (JSONArray) course.get("MandatoryPrerequisites");
+
+            if (CourseType.equals("M")) {
+                CourseType = "Mandatory";
+            } else if (CourseType.equals("E")) {
+                CourseType = "Elective";
+            }
+
+            String optionalPrerequisitesString = OptionalPrerequisites.toString().replace("\"", "").replace("[", "")
+                    .replace("]", "").replace(", ", "-");
+            String mandatoryPrerequisitesString = MandatoryPrerequisites.toString().replace("\"", "").replace("[", "")
+                    .replace("]", "").replace(", ", "-");
+
+            if (previousSemester != Semester) {
+                System.out.printf(
+                        "%n-----------------------------------------------------------------------------------------------------------------------------------------------------------------%n");
+                System.out.printf("%sSemester %s%s", Colors.RED, Semester, Colors.RESET);
+                System.out.printf("\t%s%-10s%-45s%-15s%-8s%-35s%-35s%s%n%n", Colors.YELLOW,
+                        "CourseID", "CourseName", "CourseType", "Credit",
+                        "OptionalPrerequisites", "MandatoryPrerequisites", Colors.RESET);
+                previousSemester = (int) Semester;
+            }
+
+            System.out.printf("\t\t%-10s%-45s%-15s%-8s%-35s%-35s%n",
+                    CourseID, CourseName, CourseType, Credit,
+                    optionalPrerequisitesString, mandatoryPrerequisitesString);
+
+        }
+        boolean a = true;
+        while (a) {
+            System.out.println("\nEnter '0' to go back to the Student Menu.");
+            input = new Scanner(System.in);
+            char backChoice = input.next().charAt(0);
+            if (backChoice == '0') {
+                stuMenu();
+            } else {
+                System.out.println("Invalid input. Please try again.");
+            }
+        }
+    }
+}
