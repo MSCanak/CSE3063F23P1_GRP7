87a00653556c2d883bfb79185d94d06ffc477078
Muhammed Karagul <talhakaragul@marun.edu.tr>
2023-12-11 14:46:42 +0300

    new constructers added to the Lecture Class

--- iteration2/src/Lecture.java
+++ iteration2/src/Lecture.java
@@ -4,10 +4,19 @@ public class Lecture extends Course {
 
     String lectureCourseId;
 
+    public Lecture(String courseName, String courseID, int credit, String type, int semester) {
+        super(courseName, courseID, credit, type, semester);
+    }
+
+    public Lecture(String courseName, String courseID, int credit, String type, int semester, double grade) {
+        super(courseName, courseID, credit, type, semester, grade);
+    }
+
     public Lecture(String lectureCourseId, String courseName, String courseID, String type, int credit,
-                   int theoric, int practice, int quota, ArrayList<Course> optionalPrerequisite,
-                   ArrayList<Course> mandatoryPrerequisite, ArrayList<Student> courseStudent, Lecturer lecturer) {
-        super(courseName, courseID, type, credit, theoric, practice, quota, optionalPrerequisite, mandatoryPrerequisite, courseStudent, lecturer);
+            int theoric, int practice, int quota, ArrayList<Course> optionalPrerequisite,
+            ArrayList<Course> mandatoryPrerequisite, ArrayList<Student> courseStudent, Lecturer lecturer) {
+        super(courseName, courseID, type, credit, theoric, practice, quota, optionalPrerequisite, mandatoryPrerequisite,
+                courseStudent, lecturer);
         this.lectureCourseId = lectureCourseId;
     }
 
