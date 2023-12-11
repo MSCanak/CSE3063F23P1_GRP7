2f277d9efe511041f80ad619d5b492d632984b34
kanAbdullah <abdullahkan2003@gmail.com>
2023-12-11 16:10:00 +0300

    changed constructors

--- iteration2/src/Lab.java
+++ iteration2/src/Lab.java
@@ -1,29 +1,26 @@
-public class Lab extends Course{
+import java.util.ArrayList;
 
+public class Lab extends Course{
     
     private String labCourseId;
 
-    public Lab(String labCourseId, String courseName, String courseID, int credit, String type, int semester, double grade, ArrayList<Course> optionalPrerequisite, ArrayList<Course> mandatoryPrerequisite){
-        super(courseName, courseID, credit, type, semester, grade, optionalPrerequisite, mandatoryPrerequisite);
-        this.labCourseId = labCourseId;
+    public Lab(String courseName,  String courseID, int credit, String type, int semester){
+        super(courseName, courseID, credit, type, semester);
     }
 
-    public Lab(String labCourseId, String courseName, String courseID, int credit, String type, int semester){
+    public Lab(String courseName, String courseID, int credit, String type, int semester, double grade){
         super(courseName, courseID, credit, type, semester);
-        this.labCourseId = labCourseId;
     }
     
-    public Lab(String labCourseId, String courseName, String courseID, int credit, String type, int semester, double grade){
-        super(courseName, courseID, credit, type, semester, grade);
-        this.labCourseId = labCourseId;
+    public Lab(String courseName, String courseID, CourseSession courseSession){
+        super(courseName, courseID, courseSession);
     }
 
-    public String getCourseId(){
+    public String getLabCourseId(){
         return labCourseId;
     }
     
-    public void setCourseId(int courseId){
+    public void setCourseId(String courseId){
         this.labCourseId = courseId;
-    }
-    
+    }   
 }
