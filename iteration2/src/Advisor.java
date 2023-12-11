016f497ae96626ce5c4abb4c503e577e33536b4a
Phrynidor1 <kadirbat@marun.edu.tr>
2023-12-11 22:03:57 +0300

    Constructor is changed

--- iteration2/src/Advisor.java
+++ iteration2/src/Advisor.java
@@ -5,7 +5,7 @@ class Advisor extends Lecturer {
     private ArrayList<Student> students = new ArrayList<Student>();
 
     public Advisor(String name, String surname, String email, String phoneNumber, String ID, String password, String faculty, String department, ArrayList<Course> givenCourses, String academicTitle) {
-        super(name, surname, email, phoneNumber, ID, password, faculty, department, givenCourses, academicTitle);
+        super(name, surname, email, phoneNumber, ID, password, faculty, department, academicTitle);
     }
 
 
