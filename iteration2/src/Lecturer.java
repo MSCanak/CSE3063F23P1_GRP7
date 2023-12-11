1ad92316fa0034585662799e881e04fc88acf398
Phrynidor1 <kadirbat@marun.edu.tr>
2023-12-11 09:47:25 +0300

    updating student and creating lecturer

---
+++ iteration2/src/Lecturer.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+public class Lecturer extends Person{
+
+    private ArrayList<Course> givenCourses;
+    private String academicTitle;
+
+    public Lecturer(String name, String surname, String email, String phoneNumber, String ID, String password, String faculty, String department, ArrayList<Course> givenCourses, String academicTitle ) {
+        super(name, surname, email, phoneNumber, ID, password, faculty, department);
+        this.givenCourses = givenCourses;
+        this.academicTitle = academicTitle;
+    }
+
+    public ArrayList<Course> getGivenCourses() {
+        return givenCourses;
+    }
+    public void setGivenCourses(ArrayList<Course> givenCourses) {
+        this.givenCourses = givenCourses;
+    }
+    public String getAcademicTitle() {
+        return academicTitle;
+    }   
+    public void setAcademicTitle(String academicTitle) {
+        this.academicTitle = academicTitle;
+    }
+}
--- iteration2/src/Student.java
+++ iteration2/src/Student.java
@@ -1,19 +1,13 @@
 public class Student extends Person {
-    private int semester;
+    
     private Transcript transcript;
     private Advisor advisor;
-    public Student(String name, String surname, String email, String phoneNumber, String ID, String password, String faculty, String department, int semester,Advisor advisor) {
+    public Student(String name, String surname, String email, String phoneNumber, String ID, String password, String faculty, String department,Advisor advisor) {
         super(name, surname, email, phoneNumber, ID, password, faculty, department);
-        this.semester = semester;
         this.advisor = advisor;
         this.transcript = new Transcript(this);
     }
-    public int getSemester() {
-        return semester;
-    }
-    public void setSemester(int semester) {
-        this.semester = semester;
-    }
+
     public Transcript getTranscript() {
         return transcript;
     }
