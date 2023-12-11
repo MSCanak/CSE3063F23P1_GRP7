fabf489496e89750a1a6504e76d02962775a5caf
Muhammed Karagul <talhakaragul@marun.edu.tr>
2023-12-11 20:28:10 +0300

    MessagesInterface's menus are updated

--- iteration2/src/MessagesInterface.java
+++ iteration2/src/MessagesInterface.java
@@ -3,22 +3,20 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-
-
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import java.util.Scanner;
 
-
 public class MessagesInterface {
-    Object messageObject;
-    JSONArray messageJson;
-    Session session;
 
-    ArrayList<Message> receivedMessages = new ArrayList<>();
-    ArrayList<Message> sentMessages = new ArrayList<>();
-    Scanner scanner;
+    private Object messageObject;
+    private JSONArray messageJson;
+    private Session session;
+
+    private ArrayList<Message> receivedMessages = new ArrayList<>();
+    private ArrayList<Message> sentMessages = new ArrayList<>();
+    private Scanner scanner;
 
     public MessagesInterface(Session session) {
         this.session = session;
@@ -29,112 +27,107 @@ public class MessagesInterface {
             System.out.println("Error: " + e);
         }
         scanner = new Scanner(System.in);
-        
+
     }
 
     public void messagesMenu() {
         int choice = 0;
-        while(true){
-            
-            System.out.println("Messages Menu");
-            System.out.println("1. Send Message");
-            System.out.println("2. Received Messages");
-            System.out.println("3. Sent Messages");
-            System.out.println("0. Exit");
-            System.out.print("Enter your choice: ");
+        while (true) {
+            System.out.println(Colors.RED + "\n--------------------Messages Menu--------------------\n" + Colors.RESET);
+            System.out.println(Colors.YELLOW + "1" + Colors.RESET + ".   Send Message");
+            System.out.println(Colors.YELLOW + "2" + Colors.RESET + ".   Received Messages");
+            System.out.println(Colors.YELLOW + "3" + Colors.RESET + ".   Sent Messages");
+            System.out
+                    .println(Colors.YELLOW + "*" + Colors.RESET + ".   Go back to the Course Registration System Menu");
+            System.out.println("\nWhat do you want to do?\n");
+
             choice = scanner.nextInt();
-            if(choice == 1) {
+            if (choice == 1) {
                 System.out.print("Enter receiver ID: ");
                 String receiver = scanner.next();
-                System.out.print("Enter subject: ");
+                System.out.print("\nEnter subject: ");
                 String subject = scanner.next();
-                System.out.print("Enter description: ");
+                System.out.print("\nEnter description: ");
                 String description = scanner.next();
                 newMessage(description, subject, receiver);
-            }
-            else if(choice == 2) {
+            } else if (choice == 2) {
                 showReceivedMessages();
-            }
-            else if(choice == 3) {
+            } else if (choice == 3) {
                 showSentMessages();
-            }
-            else if(choice == 0) {
+            } else if (choice == 0) {
                 return;
-            }
-            else {
-                System.out.println("Invalid choice");
+            } else {
+                System.out.println(Colors.YELLOW + "Invalid choice! Please try again." + Colors.RESET);
             }
 
         }
 
     }
-    
-    
-    public void reveiveMessagesMenu() {
+
+    private void reveiveMessagesMenu() {
         // karagül
     }
 
-    public void sentMessagesMenu() {
-        //karagül
+    private void sentMessagesMenu() {
+        // karagül
     }
 
-    public void calculateReceivedMessages() {
-        
+    private void calculateReceivedMessages() {
 
         for (Object messageObj : messageJson) {
             JSONObject message = (JSONObject) messageObj;
             String receiver = (String) message.get("Receiver");
             String sender = (String) message.get("Sender");
 
-            if(receiver.equals(session.getUser().getID())) {
+            if (receiver.equals(session.getUser().getID())) {
                 String description = (String) message.get("Description");
                 String subject = (String) message.get("Subject");
 
                 Message receivedmessage = new Message(sender, receiver, description, subject);
-                
+
                 receivedMessages.add(receivedmessage);
             }
         }
     }
 
-    public void showReceivedMessages() {
+    private void showReceivedMessages() {
         calculateReceivedMessages();
-        for(Message message : receivedMessages) {
+        for (Message message : receivedMessages) {
             System.out.println("Sender: " + message.getSenderID());
             System.out.println("Subject: " + message.getSubject());
             System.out.println("Description: " + message.getDescription());
         }
     }
 
-    public void calculateSentMessages() {
+    private void calculateSentMessages() {
 
         for (Object messageObj : messageJson) {
             JSONObject message = (JSONObject) messageObj;
             String receiver = (String) message.get("Receiver");
             String sender = (String) message.get("Sender");
-            if(sender.equals(session.getUser().getID())) {
+            if (sender.equals(session.getUser().getID())) {
                 String description = (String) message.get("Description");
                 String subject = (String) message.get("Subject");
 
                 Message sentMessage = new Message(sender, receiver, description, subject);
 
-                sentMessages.add(sentMessage);                
+                sentMessages.add(sentMessage);
             }
         }
 
     }
 
-    public void showSentMessages() {
+    private void showSentMessages() {
         calculateSentMessages();
-        for(Message message : sentMessages) {
+        for (Message message : sentMessages) {
             System.out.println("Receiver: " + message.getReceiverID());
             System.out.println("Subject: " + message.getSubject());
             System.out.println("Description: " + message.getDescription());
         }
     }
 
-    //send message specific person
-    public void newMessage(String description , String subject, String receiver) {
+    // send message specific person
+    private void newMessage(String description, String subject, String receiver) {
         JSONObject newMessage = new JSONObject();
         newMessage.put("Sender", session.getUser().getID());
         newMessage.put("Receiver", receiver);
@@ -145,13 +138,13 @@ public class MessagesInterface {
 
         try {
             PrintWriter pw = new PrintWriter("./jsons/messages.json");
-            pw.write(messageJson.toJSONString()); 
-          
-            pw.flush(); 
+            pw.write(messageJson.toJSONString());
+
+            pw.flush();
             pw.close();
 
         } catch (Exception e) {
-            System.out.println("Error: " + e);
+            System.out.println(Colors.YELLOW+"Error: " + e+Colors.RESET);
         }
     }
 }
