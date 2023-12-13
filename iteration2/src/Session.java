a28688fece075a1adb0ef070839fcc7ed777c820
Mustafa Said Çanak <mscanak0@gmail.com>
2023-12-13 15:11:39 +0300

    Session constructor updated

--- iteration2/src/Session.java
+++ iteration2/src/Session.java
@@ -6,18 +6,8 @@ public class Session {
     private LocalDateTime startingTime; // start time of the session
 
     // Constructors
-    public Session(Lecturer lecturer) {
-        this.user = lecturer;
-        this.startingTime = LocalDateTime.now();
-    }
-
-    public Session(Student student) {
-        this.user = student;
-        this.startingTime = LocalDateTime.now();
-    }
-
-    public Session(Advisor advisor) {
-        this.user = advisor;
+    public Session(Person user) {
+        this.user = user;
         this.startingTime = LocalDateTime.now();
     }
 
