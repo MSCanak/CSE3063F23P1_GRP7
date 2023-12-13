cee3ccc52bf8daf955754b6be0f074df8970d262
Muhammed Karagul <talhakaragul@marun.edu.tr>
2023-12-13 09:26:12 +0300

    CalculateWeeklySchedule implementing in processing on AdvsisorInteface

--- iteration2/src/AdvisorInterface.java
+++ iteration2/src/AdvisorInterface.java
@@ -1,48 +1,106 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class AdvisorInterface implements Schedule{
-    
+public class AdvisorInterface implements Schedule {
+
     private Scanner scanner;
     private Session session;
     private NotificationsInterface notificationsInt;
     private LoginInterface loginInt;
     private AdvisorCourseRegistrationInterface advCourseRegInt;
 
-
-    public AdvisorInterface(Session session, LoginInterface loginInt){
+    public AdvisorInterface(Session session, LoginInterface loginInt) {
         this.session = session;
         this.loginInt = loginInt;
         advCourseRegInt = new AdvisorCourseRegistrationInterface(session, this);
     }
 
-    //the terminal inteface for advisor
-    public void advMenu(){
+    // the terminal inteface for advisor
+    public void advMenu() {
 
         scanner = new Scanner(System.in);
-        System.out.print("1) Course registiration System\n*) Logout\nx) Exit\n");
-        char caseToken = scanner.next(). charAt(0);
-       
-        
-        switch(caseToken){
-            case '1': //for course Registration system
+        System.out.println(Colors.RED + "\n--------------------Advisor Menu--------------------\n" + Colors.RESET);
+        System.out.println(Colors.YELLOW + "1" + Colors.RESET + ".   View Weekly Schedule");
+        System.out.println(Colors.YELLOW + "2" + Colors.RESET + ".   View Given Courses");
+        System.out.println(Colors.YELLOW + "3" + Colors.RESET + ".   Go to Course Registration System");
+        System.out.println(Colors.YELLOW + "*" + Colors.RESET + ".   Logout");
+        System.out.println(Colors.YELLOW + "x" + Colors.RESET + ".   Exit");
+        System.out.println("\nWhat do you want to do?\n");
+
+        char caseToken = scanner.next().charAt(0);
+
+        switch (caseToken) {
+            case '1': // view weekly schedule
+                showWeeklySchedule();
+                break;
+            case '2': // view given courses
+                showGivenCourses();
+                break;
+            case '3': // go to course registration system
                 advCourseRegInt.advRegMenu();
                 break;
             case '*': // logout
                 loginInt.logout();
                 break;
-            case 'x': //exit
+            case 'x': // exit
                 loginInt.exit();
                 break;
-            default :
-                System.out.println("Please choose an action");
-                advCourseRegInt.advRegMenu();
+            default: // invalid input
+                System.out.println(Colors.YELLOW + "Invalid input! Please try again." + Colors.RESET);
+                advMenu();
+                break;
         }
     }
-    //it calculates the weekly schedule of advisor and stores it 
-    public void calculateWeeklySchedule(){}
-    //it prints the weekly schedule of advisor
-    public void showWeeklySchedule(){}
+
+    // it calculates the weekly schedule of advisor and stores it
+
+    // public ArrayList<Course> calculateWeeklySchedule() {
+    //     ArrayList<Course> weeklySchedule = new ArrayList<Course>();
+    //     ArrayList<Course> givenCourses = ((Lecturer) session.getUser()).getGivenCourses();
+    //     for (int i = 0; i < givenCourses.size(); i++) {
+    //         Course course = givenCourses.get(i);
+    //         ArrayList<Slot> slots = course.getSlots();
+    //         for (int j = 0; j < slots.size(); j++) {
+    //             Slot slot = slots.get(j);
+    //             if (slot.getDay().equals("Monday")) {
+    //                 weeklySchedule.add(course);
+    //             } else if (slot.getDay().equals("Tuesday")) {
+    //                 weeklySchedule.add(course);
+    //             } else if (slot.getDay().equals("Wednesday")) {
+    //                 weeklySchedule.add(course);
+    //             } else if (slot.getDay().equals("Thursday")) {
+    //                 weeklySchedule.add(course);
+    //             } else if (slot.getDay().equals("Friday")) {
+    //                 weeklySchedule.add(course);
+    //             }
+    //         }
+    //     }
+
+    //     // ilk önce ders kodlarını bul sonra saatlerini yerlerini filan al yazdır bir
+    //     // arraya listin içine bu şekilde showWeeklySchedule() de yazdır
+    //     return weeklySchedule;
+
+    // }
+
+    // it prints the weekly schedule of advisor
+    public void showWeeklySchedule() {
+        // // course arraylist döndürecek
+        // // ArrayList<Course> weeklySchedule = calculateWeeklySchedule();
+        // System.out.println(Colors.RED + "\n--------------------Weekly Schedule--------------------\n" + Colors.RESET);
+        // for (int i = 0; i < weeklySchedule.size(); i++) {
+        //     Course course = weeklySchedule.get(i);
+        //     System.out.println(Colors.YELLOW + (i + 1) + Colors.RESET + ". " + course.getCourseID() + " "
+        //             + course.getCourseName());
+        // }
+        // System.out.println(Colors.YELLOW + "0" + Colors.RESET + ". Back");
+        // System.out.println("\nWhich course do you want to see?\n");
+        // int choice = scanner.nextInt();
+
+    }
+
     //
-    public void showGivenCourses(){}
+    public void showGivenCourses() {
+
+    }
 }
