c9acc5a435d103bd2931c1d35acc515f7b2464d0
Muhammed Karagul <talhakaragul@marun.edu.tr>
2023-12-17 11:25:11 +0300

    Session Start Times , End Times and Session Days created at SessionTimes Class

---
+++ iteration2/src/SessionTimes.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+
+public class SessionTimes {
+    
+    public static final ArrayList<String> SESSION_START = new ArrayList<String>() {{
+        add("07:30");
+        add("08:30");
+        add("09:30");
+        add("10:30");
+        add("11:30");
+        add("13:00");
+        add("14:00");
+        add("15:00");
+        add("16:00");
+        add("17:00");
+        add("18:00");
+        add("19:00");
+        add("20:00");
+        add("21:00");
+    }};
+
+    public static final ArrayList<String> SESSION_END = new ArrayList<String>() {{
+        add("08:20");
+        add("09:20");
+        add("10:20");
+        add("11:20");
+        add("12:20");
+        add("13:50");
+        add("14:50");
+        add("15:50");
+        add("16:50");
+        add("17:50");
+        add("18:50");
+        add("19:50");
+        add("20:50");
+        add("21:50");
+
+    }};
+    public static final ArrayList<String> SESSION_DAYS = new ArrayList<String>() {{
+        add("Pazartesi");
+        add("Salı");
+        add("Çarşamba");
+        add("Perşembe");
+        add("Cuma");
+        add("Cumartesi");
+        add("Pazar");
+    }};
+}
