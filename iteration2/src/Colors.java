0e5bc9024276a40dca7cd894193f3b03ab685122
Muhammed Karagul <talhakaragul@marun.edu.tr>
2023-12-19 08:05:11 +0300

    Colors are changed to instances of Colors Class

--- iteration2/src/Colors.java
+++ iteration2/src/Colors.java
@@ -1,30 +1,62 @@
 public class Colors {
-    public static final String RESET = "\u001B[0m";
-    public static final String BLACK = "\u001B[30m";
-    public static final String RED = "\u001B[31m"; // using for title
-    public static final String GREEN = "\u001B[32m"; // using for warning
-    public static final String YELLOW = "\u001B[33m"; // using for subtitle
-    public static final String BLUE = "\u001B[34m"; //using for message
-    public static final String GRAY = "\u001B[90m"; // using for 
-    public static final String PURPLE = "\u001B[35m";
-    public static final String CYAN = "\u001B[36m";
-    public static final String WHITE = "\u001B[37m";
+    private final String RESET = "\u001B[0m";
+    private final String BLACK = "\u001B[30m";
+    private final String RED = "\u001B[31m"; // using for title
+    private final String GREEN = "\u001B[32m"; // using for warning
+    private final String YELLOW = "\u001B[33m"; // using for subtitle
+    private final String BLUE = "\u001B[34m"; // using for message
+    private final String GRAY = "\u001B[90m"; // using for
+    private final String PURPLE = "\u001B[35m";
+    private final String CYAN = "\u001B[36m";
+    private final String WHITE = "\u001B[37m";
 
-    public static final String GRAY_BACKGROUND = "\u001B[100m";//for using unread notifications and messages
-    public static final String BLACK_BACKGROUND = "\u001B[40m";
-    public static final String WHITE_BACKGROUND = "\u001B[47m";
-    public static final String RED_BACKGROUND = "\u001B[41m";//for using read notifications and messages
-    public static final String GREEN_BACKGROUND = "\u001B[42m";
-    public static final String YELLOW_BACKGROUND = "\u001B[43m";
-    public static final String BLUE_BACKGROUND = "\u001B[44m";//for using read notifications and messages
-    public static final String PURPLE_BACKGROUND = "\u001B[45m";
-    public static final String CYAN_BACKGROUND = "\u001B[46m";
+    private final String GRAY_BACKGROUND = "\u001B[100m";// for using unread notifications and messages
+    private final String BLACK_BACKGROUND = "\u001B[40m";
+    private final String WHITE_BACKGROUND = "\u001B[47m";
+    private final String RED_BACKGROUND = "\u001B[41m";// for using read notifications and messages
+    private final String GREEN_BACKGROUND = "\u001B[42m";
+    private final String YELLOW_BACKGROUND = "\u001B[43m";
+    private final String BLUE_BACKGROUND = "\u001B[44m";// for using read notifications and messages
+    private final String PURPLE_BACKGROUND = "\u001B[45m";
+    private final String CYAN_BACKGROUND = "\u001B[46m";
 
-    public static final String BOLD = "\u001B[1m";
-    public static final String UNDERLINE = "\u001B[4m";
-    
+    private final String BOLD = "\u001B[1m";
+    private final String UNDERLINE = "\u001B[4m";
+
+    public String getRESET() {
+        return RESET;
+    }
+
+    public String getRED() {
+        return RED;
+    }
+
+    public String getGREEN() {
+        return GREEN;
+    }
+
+    public String getYELLOW() {
+        return YELLOW;
+    }
+
+    public String getBLUE() {
+        return BLUE;
+    }
+
+    public String getCYAN() {
+        return CYAN;
+    }
+
+    public String getBOLD() {
+        return BOLD;
+    }
+
+    public String getUNDERLINE() {
+        return UNDERLINE;
+    }
+
     // END: abpxx6d04wxr
+
 }
 // FILEPATH: /C:/GitHub/CSE3063F23P1_GRP7/iteration2/src/colors.java
 // BEGIN: be15d9bcejpp
-
