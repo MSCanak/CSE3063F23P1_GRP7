3c869d6f2088eb70b48817f01e05c60986f023b7
Muhammed Karagul <talhakaragul@marun.edu.tr>
2024-01-03 21:01:03 +0300

    weekly schedule completed

---
+++ iteration3/src/weekly_schedule.py
@@ -0,0 +1,14 @@
+class weekly_schedule:
+    def print_courses(self, courses, courses_start_time, session_start, k):
+        result = ""
+        for i in range(len(courses)):
+            if courses_start_time[i] == session_start[k]:
+                result = courses[i]
+        return result
+
+    def print_course_places(self, course_places, courses_start_time, session_start, k):
+        result = ""
+        for i in range(len(course_places)):
+            if courses_start_time[i] == session_start[k]:
+                result = course_places[i]
+        return result
