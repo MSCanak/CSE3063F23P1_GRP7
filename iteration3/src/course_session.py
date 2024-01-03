da8c3f45667a8ce9d7f4c428403c4d5d06633c38
Muhammed Karagul <talhakaragul@marun.edu.tr>
2024-01-03 21:01:20 +0300

    course session completed

---
+++ iteration3/src/course_session.py
@@ -0,0 +1,30 @@
+class course_session:
+    def __init__(self, course_day, course_start_time, course_end_time, course_place):
+        self.course_day = course_day
+        self.course_start_time = course_start_time
+        self.course_end_time = course_end_time
+        self.course_place = course_place
+
+    def get_course_day(self):
+        return self.course_day
+
+    def set_course_day(self, course_day):
+        self.course_day = course_day
+
+    def get_course_start_time(self):
+        return self.course_start_time
+
+    def set_course_start_time(self, course_start_time):
+        self.course_start_time = course_start_time
+
+    def get_course_end_time(self):
+        return self.course_end_time
+
+    def set_course_end_time(self, course_end_time):
+        self.course_end_time = course_end_time
+
+    def get_course_place(self):
+        return self.course_place
+
+    def set_course_place(self, course_place):
+        self.course_place = course_place
