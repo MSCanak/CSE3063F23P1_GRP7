ce01fea2cd4113fa70c71ab00a25c7d9d4399f98
Muhammed Nael <imohmad7@gmail.com>
2024-01-04 22:00:48 +0300

    fixed importing in session.py

--- iteration3/src/session.py
+++ iteration3/src/session.py
@@ -1,15 +1,17 @@
+from person import Person
+
 from datetime import datetime
 
 class Session:
-    def __init__(self, user):
-        self.user = user
-        self.starting_time = datetime.now()
+    def __init__(self, user: Person):
+        self.__user = user
+        self.__starting_time = datetime.now()
 
-    def get_startingTime(self):
-        return self.starting_time
+    def get_starting_time(self):
+        return self.__starting_time
 
     def get_user(self):
-        return self.user
+        return self.__user
 
-    def set_user(self, user):
-        self.user = user
+    def set_user(self, user: Person):
+        self.__user = user
