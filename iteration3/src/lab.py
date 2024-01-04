28fa7a1927d22d5ac4068c1074fa1650038c9c85
feyzobey <feyzullahasillioglu@marun.edu.tr>
2024-01-04 23:20:23 +0300

    Refactor Course subclasses' constructors and add their data types

--- iteration3/src/lab.py
+++ iteration3/src/lab.py
@@ -1,14 +1,16 @@
 from course import Course
-class Lab(Course):
-    def __init__(self, course_name, lab_id, *args):
-        super().__init__(*args)
-        self.__lab_id = lab_id
-        self.__last_index = lab_id.index(".")
-        self__course_id = lab_id[:self.__last_index]
-        self.set_course_id(self__course_id)
 
-    def get_lab_id(self):
+
+class Lab(Course):
+    def __init__(self, course_name: str, lab_id: str, *args):
+        super().__init__(course_name=course_name, *args)
+        last_index = lab_id.index(".")
+        course_id = lab_id[:last_index]
+        self.set_course_id(course_id)
+        self.__lab_id = lab_id
+
+    def get_lab_id(self) -> str:
         return self.__lab_id
 
-    def set_lab_id(self, course_id):
-        self.__lab_id = course_id
+    def set_lab_id(self, lab_id: str):
+        self.__lab_id = lab_id
--- iteration3/src/lecture.py
+++ iteration3/src/lecture.py
@@ -1,14 +1,16 @@
 from course import Course
+
+
 class Lecture(Course):
-    def __init__(self, course_name, lecture_id, *args):
-        super().__init__(*args)
-        self.__lecture_id = lecture_id
+    def __init__(self, course_name: str, lecture_id: str, *args):
+        super().__init__(course_name=course_name, *args)
         last_index = lecture_id.index(".")
         course_id = lecture_id[:last_index]
         self.set_course_id(course_id)
+        self.__lecture_id = lecture_id
 
-    def get_lecture_id(self):
+    def get_lecture_id(self) -> str:
         return self.__lecture_id
 
-    def set_lecture_id(self, lecture_id):
+    def set_lecture_id(self, lecture_id: str):
         self.__lecture_id = lecture_id
